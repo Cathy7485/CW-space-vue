6ad0755918b78596f8365e8e6c97744a6a81cb0e
Cathy L <cathy141153@gmail.com>
2024-06-01 08:59:36 +0800

    git commit -m "feaT:新增loading套件"

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import { createPinia } from 'pinia';
 
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import Loading from 'vue3-loading-overlay';
+import 'vue3-loading-overlay/dist/vue3-loading-overlay.css';
 import App from './App.vue';
 import router from './router';
 import 'bootstrap';
 
 const app = createApp(App);
 
+app.component('LoadingComponent', Loading);
 app.use(createPinia());
 app.use(router);
 app.use(VueAxios, axios);
