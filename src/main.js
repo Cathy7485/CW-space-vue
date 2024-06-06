596599702c9a7dbf6255fb01bda52544a61dc477
Cathy L <cathy141153@gmail.com>
2024-06-06 22:22:21 +0800

    feat:新增聯絡我們表單及表單驗證套件

--- src/main.js
+++ src/main.js
@@ -4,15 +4,40 @@ import { createPinia } from 'pinia';
 
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+
 import Loading from 'vue3-loading-overlay';
 import 'vue3-loading-overlay/dist/vue3-loading-overlay.css';
-import App from './App.vue';
+
+import {
+  Field, Form, ErrorMessage, defineRule, configure,
+} from 'vee-validate';
+import { required, email } from '@vee-validate/rules';
+import { localize, setLocale } from '@vee-validate/i18n';
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json';
+
 import router from './router';
 import 'bootstrap';
+
+import App from './App.vue';
+
+defineRule('email', email);
+defineRule('required', required);
+
+configure({
+  generateMessage: localize({ zh_TW: zhTW }), // 載入繁體中文語系
+  validateOnInput: true, // 當輸入任何內容直接進行驗證
+});
+
+// 設定預設語系
+setLocale('zh_TW');
 
 const app = createApp(App);
 
 app.component('LoadingComponent', Loading);
+app.component('VueLoading', Loading);
+app.component('VForm', Form);
+app.component('VField', Field);
+app.component('ErrorMessage', ErrorMessage);
 app.use(createPinia());
 app.use(router);
 app.use(VueAxios, axios);
