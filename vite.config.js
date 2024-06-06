d064ae82e18faa7dacbc69e026682b6cccc5f97c
Cathy L <cathy141153@gmail.com>
2024-06-06 22:22:55 +0800

    git commit -m "feat:修改網址路徑參數"

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/CW-space-vue/',
+  base: process.env.NODE_ENV === 'production' ? '/CW-space-vue/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
