0f82e37e75cccf0369929c170695d1c77f3815f3
Cathy L <cathy141153@gmail.com>
2024-10-18 14:55:36 +0800

    feat:調整後台header結構及樣式排版

--- src/components/AdminHeader.vue
+++ src/components/AdminHeader.vue
@@ -1,9 +1,16 @@
 <template>
-  <router-link to="/products">產品列表</router-link>
-  <a
-    href="#"
-    @click.prevent="logout"
-    class="button primary">登出</a>
+  <div class="p-4 bg-secondary mb-5">
+    <div class="container d-flex justify-content-between align-items-center">
+      <div class="admin-links">
+        <router-link to="/products">產品列表</router-link>
+        <router-link to="/article">文章列表</router-link>
+      </div>
+      <a
+        href="#"
+        @click.prevent="logout"
+        class="button primary">登出</a>
+    </div>
+  </div>
 </template>
 
 <script>
