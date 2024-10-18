ba74ec997862aed8a5da808dad330c769cce0432
Cathy L <cathy141153@gmail.com>
2024-10-18 15:00:41 +0800

    feat:空間資料增加loading樣式，空間標題新增切換樣式

--- src/stores/spaceDataStore.js
+++ src/stores/spaceDataStore.js
@@ -27,14 +27,27 @@ const spaceStore = defineStore('spaceStore', {
     },
     getSpace(id) {
       const url = `${VITE_URL}api/${VITE_PATH}/product/${id}`;
+      status.isLoading = true;
       axios
         .get(url)
         .then((res) => {
+          status.isLoading = false;
           this.thisSpace = res.data.product;
         })
         .catch((err) => {
           console.log(err.response.data.message);
         });
+    },
+    spaceTitleClass() {
+      let target;
+      this.spaceList.forEach((element) => {
+        target = element;
+        if (element.title === this.thisSpace.title) {
+          target.isActive = true;
+        } else {
+          target.isActive = false;
+        }
+      });
     },
   },
 });
