1553e03799dbd21abef5cae3ca71af75e77fa091
Cathy L <cathy141153@gmail.com>
2024-11-07 09:59:20 +0800

    feat: 調整db.json資料後，需調整串接space資料名稱，且將其改為composition api (將swiper暫時刪除)

--- src/views/frontend/SpaceView.vue
+++ src/views/frontend/SpaceView.vue
@@ -1,3 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useSpaceStore } from '@/stores/spaceStore';
+import PageBanner from '@/components/PageBanner.vue';
+import bannerUrl from '@/assets/images/shared-banner.jpg';
+
+const pageTitle = ref('空間介紹');
+
+const store = useSpaceStore();
+const { spaceList } = storeToRefs(store);
+</script>
+
 <template>
   <PageBanner
     :images-url="bannerUrl"
@@ -9,21 +22,21 @@
         :key="item.id"
         class="space-tab-item"
         :class="{'active': item.isActive }"
-        @click="changeSpace(item)">{{ item.title }}</div>
+        @click="changeSpace(item)">{{ item.name }}</div>
     </div>
     <div class="row">
       <div class="col-lg-5 d-flex flex-column">
-        <div class="space-title">{{ tempData.title }}</div>
+        <div class="space-title"></div>
         <div class="d-block mb-3">
           <span>開放時間：</span>
-          <p>{{ tempData.description }}</p>
+          <p></p>
           <span>費用：</span>
-          <span v-if="tempData.price === 0">免費</span>
-          <span class="fw-bold" v-else>NT ${{ tempData.price }}元</span>
+          <span>免費</span>
+          <span class="fw-bold">NT $元</span>
         </div>
         <div class="space-page-info">
           <div class="title">詳細資訊</div>
-          <div class="p-3" v-html="tempData.content"></div>
+          <div class="p-3"></div>
         </div>
         <div class="mt-auto">
           <router-link
@@ -35,7 +48,12 @@
         </div>
       </div>
       <div class="col-lg-7">
-        <swiper
+        <swiper-container>
+          <swiper-slide>Slide 1</swiper-slide>
+          <swiper-slide>Slide 2</swiper-slide>
+          <swiper-slide>Slide 3</swiper-slide>
+        </swiper-container>
+        <!-- <swiper
         :slides-per-view="1"
         :space-between="20"
         :modules="modules"
@@ -48,72 +66,8 @@
               <img :src="i" class="w-100" alt="">
             </div>
           </swiper-slide>
-        </swiper>
+        </swiper> -->
       </div>
     </div>
   </div>
 </template>
-
-<script>
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Navigation } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import PageBanner from '@/components/PageBanner.vue';
-import bannerUrl from '@/assets/images/shared-banner.jpg';
-import { mapActions, mapState } from 'pinia';
-import spaceStore from '@/stores/spaceDataStore';
-
-export default {
-  data() {
-    return {
-      isActive: false,
-      bannerUrl,
-      pageTitle: '環境空間',
-      tempData: {
-        title: '共享辦公空間',
-        price: '300',
-        content: '<p>- 獨立寬敞座位<br>- 穩定高速wifi、獨立插座<br>- 適合SOHO族、一人公司者<br>- 公共空間設備皆可自由使用</p>',
-        description: '週一至週五 8:00-18:00（不含例假日)',
-        imagesUrl: [
-          'https://github.com/Cathy7485/CW-space-vue/blob/main/src/assets/images/shared02.jpg?raw=true',
-        ],
-      },
-      modules: [Navigation],
-    };
-  },
-  components: {
-    Swiper,
-    SwiperSlide,
-    PageBanner,
-  },
-  methods: {
-    ...mapActions(spaceStore, ['getSpaceList']),
-    spaceTitleClass() {
-      let target;
-      this.spaceList.forEach((element) => {
-        target = element;
-        if (element.title === this.tempData.title) {
-          target.isActive = true;
-        } else {
-          target.isActive = false;
-        }
-      });
-    },
-    changeSpace(item) {
-      this.tempData = item;
-      this.spaceTitleClass();
-    },
-  },
-  computed: {
-    ...mapState(spaceStore, ['spaceList']),
-  },
-  mounted() {
-    this.getSpaceList();
-  },
-  updated() {
-    // 取完資料再增加樣式
-    this.spaceTitleClass();
-  },
-};
-</script>
