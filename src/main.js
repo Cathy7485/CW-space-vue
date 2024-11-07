6411411be0893086580cfc7c294922ccd3fa2d1a
Cathy L <cathy141153@gmail.com>
2024-11-07 09:34:51 +0800

    feat:重新安裝swiper改為全域註冊

--- src/main.js
+++ src/main.js
@@ -1,45 +1,18 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-
-import axios from 'axios';
-import VueAxios from 'vue-axios';
-
-import Loading from 'vue3-loading-overlay';
-import 'vue3-loading-overlay/dist/vue3-loading-overlay.css';
-
-import {
-  Field, Form, ErrorMessage, defineRule, configure,
-} from 'vee-validate';
-import { required, email } from '@vee-validate/rules';
-import { localize, setLocale } from '@vee-validate/i18n';
-import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json';
-
+import { register } from 'swiper/element/bundle';
+import { timeformat } from './directive/timeformat';
+import App from './App.vue';
 import router from './router';
 import 'bootstrap';
 
-import App from './App.vue';
+register();
 
-defineRule('email', email);
-defineRule('required', required);
-
-configure({
-  generateMessage: localize({ zh_TW: zhTW }), // 載入繁體中文語系
-  validateOnInput: true, // 當輸入任何內容直接進行驗證
-});
-
-// 設定預設語系
-setLocale('zh_TW');
-
+const pinia = createPinia();
 const app = createApp(App);
 
-app.component('LoadingComponent', Loading);
-app.component('VueLoading', Loading);
-app.component('VForm', Form);
-app.component('VField', Field);
-app.component('ErrorMessage', ErrorMessage);
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
-app.use(VueAxios, axios);
 
+app.directive('timeformat', timeformat);
 app.mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,13 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        isCustomElement: (tag) => tag.includes('swiper'),
+      },
+    },
+  })],
   base: process.env.NODE_ENV === 'production' ? '/CW-space-vue/' : '/',
   resolve: {
     alias: {
