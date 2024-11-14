961c48fcacd45ed6c22c3117bfc8c27af7566ea8
Cathy L <cathy141153@gmail.com>
2024-11-14 09:28:07 +0800

    feat: 常見問題頁面新增注意事項

--- src/views/frontend/FaqView.vue
+++ src/views/frontend/FaqView.vue
@@ -12,6 +12,15 @@ const pageTitle = ref('常見問題');
     :images-url="bannerUrl"
     :page-title="pageTitle"></PageBanner>
   <div class="container block mt-5">
+    <h3 class="mb-3 fw-bold">常見問題</h3>
     <faq />
+    <h3 class="mb-3 fw-bold">注意事項</h3>
+    <ul class="notice-list">
+      <li>1. 共享空間歡迎各新創公司、個人工作室、SOHO族入駐使用，不接受博弈產業公司類型入駐或承租，敬請見諒</li>
+      <li>2. 入駐共同工作空間需要繳保證金兩個月租金。保證金於入駐當天會轉為押金，並在結束合約當日檢查無毀損問題時返還</li>
+      <li>3. 公共區域應保持輕聲細語，請勿大聲喧嘩，打擾到其他人員</li>
+      <li>4. 承租空間須自行清潔環境，請愛惜共享空間內所有設備及裝潢，若發現造成家具或設備損毀，需額外支付維護費用</li>
+      <li>5. 公共空間會有專人定期打掃整潔，請各入駐夥伴做好資源回收分類即可</li>
+    </ul>
   </div>
 </template>
