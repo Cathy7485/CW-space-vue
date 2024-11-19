5d1eddf2ea2e4dcd9ee708389d4f02eda0239f8c
Cathy L <cathy141153@gmail.com>
2024-11-19 14:58:22 +0800

    feat: 因修改db.json，調整資料渲染呈現

--- src/components/SwiperPlan.vue
+++ src/components/SwiperPlan.vue
@@ -38,25 +38,27 @@ onMounted(() => {
           </div>
           <div class="info">
             <div class="title">{{ item.name }}</div>
-            <div
-              v-for="price in item.price"
-              :key="price"
-            >
-              <div v-for="item in Object.keys(price)" :key="item">
-                <div v-if="item === 'day'">
-                  當日體驗 NT$ {{ Object.values(price)[0] }} / 天
-                </div>
-                <div v-if="item === 'month'">
-                  整月優惠 NT$ {{ Object.values(price)[0].length > 0
-                    ? '6000 起' : Object.values(price)[0] }} / 月
-                </div>
-                <div v-if="item === 'hour'">
-                  NT$ {{ Object.values(price)[0] }} / 時
-                </div>
-                <div v-if="item === 'free'">免費使用</div>
-                <div v-else></div>
+            <div v-if="item.type.length > 1">
+              <div v-if="Object.keys(item.type[0].price)[0] === 'day'">
+                當日體驗 NT$ {{ Object.values(item.type[0].price)[0] }} / 天
               </div>
+              <div v-if="Object.keys(item.type[0].price)[0] === 'month'">
+                整月優惠 NT$ {{ Object.values(item.type[0].price)[0].length > 0
+                  ? '6000 起' : Object.values(item.type[0].price)[0] }} / 月
+              </div>
+              <div v-if="Object.keys(item.type[0].price)[0] === 'hour'">
+                NT$ {{ Object.values(item.type[0].price)[0] }} / 時
+              </div>
+              <div v-else></div>
             </div>
+            <template v-else>
+              <div
+                v-for="price in item.type"
+                :key="price"
+              >
+                <div v-if="Object.values(price.price)[0] === 0">免費使用</div>
+              </div>
+            </template>
             <hr>
             <div>
               <p>{{ item.info }}</p>
