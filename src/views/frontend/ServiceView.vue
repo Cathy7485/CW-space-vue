7232d27e272ab72b021ca3ae0316598a44a98d08
Cathy L <cathy141153@gmail.com>
2024-11-19 14:32:24 +0800

    feat:加上日租、月租、年租tab切換按鈕

--- src/views/frontend/ServiceView.vue
+++ src/views/frontend/ServiceView.vue
@@ -4,7 +4,11 @@ import bannerUrl from '@/assets/images/plan-banner.jpg';
 import PageBanner from '@/components/PageBanner.vue';
 import ServicePlan from '@/components/SwiperPlan.vue';
 
+const activeIdx = ref(0);
+const spacePlan = ref(['日租方案', '月租方案', '年租方案']);
 const pageTitle = ref('方案介紹');
+
+const changeIdx = (idx) => { activeIdx.value = idx; };
 </script>
 
 <template>
@@ -12,6 +16,14 @@ const pageTitle = ref('方案介紹');
     :images-url="bannerUrl"
     :page-title="pageTitle"></PageBanner>
   <div class="block container mt-5">
+    <div class="space-tabs">
+      <div
+        v-for="(item, index) in spacePlan"
+        :key="item"
+        class="space-tab-item"
+        :class="{'active': index === activeIdx }"
+        @click="changeIdx(index)">{{ item }}</div>
+    </div>
     <ServicePlan />
   </div>
 </template>
