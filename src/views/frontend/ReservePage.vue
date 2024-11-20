e7e86d3bcfc528c51b61ecb517c60ee55dd619f8
Cathy L <cathy141153@gmail.com>
2024-11-20 11:55:45 +0800

    feat: 預約頁面新增tab細項按鈕

--- src/views/frontend/ReservePage.vue
+++ src/views/frontend/ReservePage.vue
@@ -4,6 +4,10 @@ import PageBanner from '@/components/PageBanner.vue';
 import bannerUrl from '@/assets/images/visit-banner.jpg';
 
 const pageTitle = ref('聯絡我們-預約參觀');
+const activeIdx = ref(0);
+const contactTabs = ref(['預約參觀', '預約空間', '線上留言', '地理位置']);
+
+const changeIdx = (idx) => { activeIdx.value = idx; };
 </script>
 
 <template>
@@ -11,6 +15,13 @@ const pageTitle = ref('聯絡我們-預約參觀');
     :images-url="bannerUrl"
     :page-title="pageTitle"></PageBanner>
   <div class="container block mt-5">
-    預約
+    <div class="space-tabs">
+      <div
+        v-for="(item, index) in contactTabs"
+        :key="item"
+        class="space-tab-item"
+        :class="{'active': index === activeIdx }"
+        @click="changeIdx(index)">{{ item }}</div>
+    </div>
   </div>
 </template>
