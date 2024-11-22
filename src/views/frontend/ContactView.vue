d31780aef438e845c0637d235ac080684668294f
Cathy L <cathy141153@gmail.com>
2024-11-22 09:36:09 +0800

    feat: 聯絡我們頁面新增地理位置內容

--- src/views/frontend/ContactView.vue
+++ src/views/frontend/ContactView.vue
@@ -21,6 +21,24 @@ const changeIdx = (idx) => { activeIdx.value = idx; };
       <div v-for="(item, index) in contactTabs" :key="item" class="space-tab-item"
         :class="{ 'active': index === activeIdx }" @click="changeIdx(index)">{{ item }}</div>
     </div>
-    <PageContact />
+    <PageContact v-if="activeIdx === 0" />
+    <div v-else class="location-section">
+      <div class="left">
+        <div class="fs-4 fw-bold">地理位置</div>
+        <div class="fw-bold mt-4">臺南市安平區永華路二段777號</div>
+        <hr>
+        <div>搭乘台南捷運綠線到府平公園站下車，步行3分鐘即可抵達CW space 中心</div>
+      </div>
+      <div class="right">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.890343628232!2d120.16842215129887!3d22.991059489200016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e7608a0982b33%3A0x1e55b069e1044555!2zNzA45Y-w5Y2X5biC5a6J5bmz5Y2A5rC46I-v6Lev5LqM5q61Nzc36Jmf!5e0!3m2!1szh-TW!2stw!4v1732238779614!5m2!1szh-TW!2stw"
+          width="100%"
+          height="100%"
+          style="border:0;"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+    </div>
   </div>
 </template>
