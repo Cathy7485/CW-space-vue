2c4e3ad191fd6cc93267641b9074ddaaf071364b
Cathy L <cathy141153@gmail.com>
2024-12-18 07:40:03 +0800

    deploy

--- assets/FrontLayout-B8W7zgGG.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as o,a,u as t,F as r,R as n}from"./index-CdfByT6K.js";import{H as s,_ as m,F as _,a as c}from"./GoTop-CuZ6J7qW.js";import"./statusStore-Brttbbxo.js";const d={__name:"FrontLayout",setup(p){return(u,f)=>(e(),o(r,null,[a(s),a(m),a(t(n)),a(_),a(c)],64))}};export{d as default};
---
+++ assets/statusStore-xlRvbY1f.js
@@ -0,0 +1,1 @@
+import{x as a,e as t}from"./index-C52U05r6.js";const r=a("statusStore",()=>{const s=t(!1),e=t(!1);return{loadingActive:o=>{e.value=o},isFullPage:s,isLoading:e}});export{r as u};
