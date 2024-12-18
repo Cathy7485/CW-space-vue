82311d06bb7fdd5631eb103f875a82e13cad6ea3
Cathy L <cathy141153@gmail.com>
2024-12-18 20:40:36 +0800

    feat: 將後臺-預約參觀管理、預約空間管理，改為動態路由，以便查看單筆資料細節

--- src/views/backend/reserve/Index.vue
+++ src/views/backend/reserve/Index.vue
@@ -1,24 +1,25 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
 const { VITE_DATA_URL } = import.meta.env;
-const visitData = ref([]);
+const reserveData = ref([]);
 
-const getVisit = async () => {
-  const api = `${VITE_DATA_URL}/visit`;
+const getReserve = async () => {
+  const api = `${VITE_DATA_URL}/reserves`;
   const res = await axios.get(api);
-  visitData.value = res.data;
+  reserveData.value = res.data;
 };
 
 onMounted(() => {
-  getVisit();
+  getReserve();
 });
 </script>
 
 <template>
   <div>
-    預約參觀
+    預約空間
   </div>
   <div class="data-list">
     <table class="table table-striped align-middle">
@@ -27,20 +28,25 @@ onMounted(() => {
           <td>填寫時間</td>
           <td>姓名</td>
           <td>公司名稱</td>
-          <td>連絡電話</td>
-          <td>email</td>
+          <!-- <td>連絡電話</td>
+          <td>email</td> -->
+          <td>方案</td>
           <td>空間</td>
-          <td>參觀時段</td>
+          <td>空間分類</td>
+          <td>預約時間</td>
           <td></td>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="item in visitData" :key="item.id">
+        <tr v-for="item in reserveData" :key="item.id">
           <td>{{ item.appointment }}</td>
+          <td>{{ item.name }}</td>
           <td>{{ item.company }}</td>
-          <td>{{ item.phone }}</td>
-          <td>{{ item.email }}</td>
+          <!-- <td>{{ item.phone }}</td>
+          <td>{{ item.email }}</td> -->
+          <td>{{ item.plan }}</td>
           <td>{{ item.space }}</td>
+          <td>{{ item.kind }}</td>
           <td>{{ item.time }}</td>
           <td class="text-center">
             <button
