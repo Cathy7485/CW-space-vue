6a831171216b8b25613b7bbd1acce7c2bbd7f0ad
Cathy L <cathy141153@gmail.com>
2025-01-17 15:06:39 +0800

    feat: 新增404頁面樣式

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    404
+  <div class="error-page">
+    <div class="error-title">404</div>
+    <div class="error">ERROR</div>
+    <div class="back-home">回首頁</div>
   </div>
 </template>
